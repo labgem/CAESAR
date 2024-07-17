4259ea730d75d57f3766cf2c3752774f10bd1523
thomas-bailly <thomas.bailly1698@gmail.com>
2024-07-17 08:28:40 +0200

    add: a script used only with a slurm sbatch command

---
+++ src/phylo.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+echo $(date)
+SECONDS=0
+
+outdir=""
+
+Help(){
+    echo "Script used only with a slurm sbatch command"
+    echo
+    echo "usage: phylo.sh <path>/phylo.py [-h] [-o] -f [-c]"
+    echo
+    echo "options:"
+    echo "  -h    show this help message"
+    echo "  -o    output directory [default: './']"
+    echo "  -f    fasta file containing all candidates sequences"
+    echo "  -c    clusters tsv file"
+}
+
+export -f Help
+
+while getopts ":o:f:c:" option
+do
+    case $option in
+        o)
+            outdir=$OPTARG
+            ;;
+        f)
+            fasta=$OPTARG
+            ;;
+        c)
+            cluster=$OPTARG
+            ;;
+        :)
+            Help
+            exit 1
+            ;;
+        \?)
+            Help
+            exit 1
+            ;;
+        *)
+            Help
+            exit 1
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
+script_path=$1
+
+command=$(echo $script_path -f $fasta)
+
+if [ ! -f $outdir ] && [ $outdir != "./" ]
+then
+    command=$(echo $command -o $outdir)
+    mkdir $outdir
+fi
+
+if [ ! -z $cluster ]
+then
+    command=$(echo $command --clusters $cluster)
+fi
+
+python $command
